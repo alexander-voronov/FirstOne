c098f2480f96aa32b155ec485f95944f52aa7dbe
Alexander Voronov <dr.strum@gmail.com>
2020-04-08 15:53:41 +0300

    third commit

--- src/main/java/ru/geekbrains/lesson1/FirstApp.java
+++ src/main/java/ru/geekbrains/lesson1/FirstApp.java
@@ -4,5 +4,6 @@ public class FirstApp {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("TEST");
+        System.out.println("test2");
     }
 }
